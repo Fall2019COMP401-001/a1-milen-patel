8ca572a0ef49ed7c35c4e93a1b4786f9ec99b04c
milenpatel <milenpatel@milens-mbp.wireless-1x.unc.edu>
2019-08-27 22:24:32 -0400

    Implemented A1Jedi; Added documentation for A1Adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,7 +1,5 @@
 package a1;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class A1Adept {
@@ -9,17 +7,21 @@ public class A1Adept {
 	public static void main(String[] args) {
 		//Create a scanner to read the user input
 		Scanner userInput = new Scanner(System.in);
-		//Create a map to model the inventory of good and their prices
-		Map<String, Double> Inventory = new HashMap<String, Double>();
-		//Start by getting the number of items that will be in the inventory map
+		//Start by getting the number of items that will be in the inventory arrays
 		int NUM_OF_INVENTORY_ITEMS = userInput.nextInt();
-		//Now, add all NUM_OF_INVENTORY_ITEMS to the Inventory map
+		//Create two arrays to model the inventory of goods in the store
+		String[] INVENTORY_ITEMS = new String[NUM_OF_INVENTORY_ITEMS];
+		double[] INVENTORY_PRICES = new double[NUM_OF_INVENTORY_ITEMS];
+		
+		//Now, add all NUM_OF_INVENTORY_ITEMS to the two inventory arrays
 		for(int i = 0; i < NUM_OF_INVENTORY_ITEMS; i++) {
-			//Add the item and its associated cost to the map
-			Inventory.put(userInput.next(), userInput.nextDouble());
+			//Add the item name
+			INVENTORY_ITEMS[i] = userInput.next();
+			//Add the item cost
+			INVENTORY_PRICES[i] = userInput.nextDouble();
 		}
 		
-		//Now, create a variable to get the number of customers that will be accounted
+		//Now, create a variable to get the number of customers that will be accounted for
 		int NUMBER_OF_CUSTOMERS = userInput.nextInt();
 		
 		//Now, create an array to model all of the customer names
@@ -35,15 +37,26 @@ public class A1Adept {
 			int NUMBER_OF_ITEMS_PURCHASED = userInput.nextInt();
 			//Now loop over each item purchased and add its price to the table
 			for (int j = 0; j < NUMBER_OF_ITEMS_PURCHASED; j++) {
-				CUSTOMER_TOTALS[i] += userInput.nextInt()*Inventory.get(userInput.next());
+				CUSTOMER_TOTALS[i] += userInput.nextInt()*FindItemPrice(userInput.next(),INVENTORY_ITEMS,INVENTORY_PRICES);
 			}
 		}
-		
+		//Call the three functions implemented below to display the targeted A1Adept output
 		FindBiggestSpender(CUSTOMER_NAMES, CUSTOMER_TOTALS);
 		FindLowestSpender(CUSTOMER_NAMES, CUSTOMER_TOTALS);
 		ComputeAverageSpending(CUSTOMER_TOTALS);
 	}
 	
+	/* FindBiggestSpender
+	 * Prints the name and transaction total of the customer who spent the most amount of money
+	 * 
+	 * Input: An array of strings containing all the numbers of the customers (name)
+	 * 		  An array of doubles containing all the transaction totals for each customer (totals)
+	 * 
+	 * Output: A summary line containing the name and transaction total of the customer who spent the most amount of money
+	 * 
+	 * Precondition: Both arrays are populated and for valid i names[i] corresponds to totals[i]
+	 * 
+	 */
 	public static void FindBiggestSpender(String[] names, double[] totals) {
 		//Loop over all the entries in the totals array
 		int highestIndex = 0;
@@ -56,22 +69,72 @@ public class A1Adept {
 		System.out.println("Biggest: " + names[highestIndex] + " (" + String.format("%.2f", totals[highestIndex]) + ")");
 	}
 	
+	/* FindLowestSpender
+	 * Prints the name and transaction total of the customer who spent the least amount of money
+	 * 
+	 * Input: An array of strings containing all the numbers of the customers (name)
+	 * 		  An array of doubles containing all the transaction totals for each customer (totals)
+	 * 
+	 * Output: A summary line containing the name and transaction total of the customer who spent the least amount of money
+	 * 
+	 * Precondition: Both arrays are populated and for valid i names[i] corresponds to totals[i]
+	 * 
+	 */
 	public static void FindLowestSpender(String[] names, double[] totals) {
 		//Loop over all the entries in the totals array
-		int lowestIndex = 0;
+		int lowestIndex = 0; //Have a variable to keep track of the index of the customer who spent the least amount
 		for (int i = 0; i < totals.length; i++) {
 			if (totals[i] < totals[lowestIndex]) {
-				lowestIndex = i;
+				lowestIndex = i; //If this customer spent less than the current lowest, then the current lowest becomes this customer
 			}
 		}
 		//Print a summary line to the screen
 		System.out.println("Smallest: " + names[lowestIndex] + " (" + String.format("%.2f", totals[lowestIndex]) + ")");
 	}
+	
+	/* ComputeAverageSpending
+	 * Finds the average spent by all customers
+	 * 
+	 * Input: An array of all the customer totals (spendingValues)
+	 * 
+	 * Output: A line printed to the console with the average transaction total for all customers
+	 * 
+	 * Precondition: spendingValues is populated correctly
+	 * 
+	 */
 	public static void ComputeAverageSpending(double[] spendingValues) {
-		double total = 0;
+		double total = 0; //Keep a running total
 		for (int i = 0; i < spendingValues.length; i++) {
-			total += spendingValues[i];
+			total += spendingValues[i]; //Increment the running total by the transaction amount of the current customer
 		}
+		//Print the final message while dividing the running total by the number of customers
 		System.out.println("Average: " + String.format("%.2f",total/spendingValues.length));
+	}
+	
+	/* FindItemPrice
+	 * Finds the price of an item given its associated name
+	 * 
+	 * Input: A string representing the name of an item (itemName)
+	 * 		  An array of strings representing the names of all items in the inventory (names)
+	 * 		  An array of doubles representing the cost of all items in the inentory (prices)
+	 * 
+	 * Output: A double representing the cost of the item with name 'itemName'
+	 * 
+	 * Precondition:
+	 * itemName, names, and prices are all populated
+	 * itemName is a valid item that is in the inventory of 'names' and has an associated 'prices' value
+	 */
+	public static double FindItemPrice(String itemName, String[] names, double[] prices) {
+		//Loop over all the items in names and find a matching item name
+		int nameIndex = 0;
+		for (int i = 0; i < names.length; i++) {
+			if (names[i].equals(itemName)) {
+				//if the name matches the target name, then get the index value and leave
+				nameIndex = i;
+				break;
+			}
+		}
+		//Now return the price of the targeted index value
+		return prices[nameIndex];
 	}
 }
--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -5,10 +5,134 @@ import java.util.Scanner;
 public class A1Jedi {
 
 	public static void main(String[] args) {
+		//Create a scanner to read the user input
+		Scanner userInput = new Scanner(System.in);
 		
-		Scanner scan = new Scanner(System.in);
+		//Start by getting the number of items that will be in the inventory arrays
+		int NUM_OF_INVENTORY_ITEMS = userInput.nextInt();
+		//Create two arrays to model the inventory of goods in the store
+		//INVENTORY_ITEMS[i] will contain the name of the ith item in the store (starting from i=0)
+		//INVENTORY_PRICES[i] will contain the price of the ith item in the store (starting from i=0)
+		String[] INVENTORY_ITEMS = new String[NUM_OF_INVENTORY_ITEMS];
+		double[] INVENTORY_PRICES = new double[NUM_OF_INVENTORY_ITEMS];
+		//Create two additional arrays for storing purchase statistics
+		//UNIQUE_CUSTOMERS[i] will contain the number of unique customers that have purchased INVENTORY_ITEMS[i]
+		//QUANTITY_PURCHASED[i] will the contian the number of units that have been purchased of INVENTORY_ITEMS[i]
+		int[] UNIQUE_CUSTOMERS = new int[NUM_OF_INVENTORY_ITEMS];
+		int[] QUANTITY_PURCHASED = new int[NUM_OF_INVENTORY_ITEMS]; 
+		
+		//As a side note all four arrays will contain data about the same object given a consistent position
+		//INVENTORY_ITEMS, INVENTORY_PRICES, UNIQUE_CUSTOMERS, QUANTITY_PURCHASED are all related
+		
+		
+		//Now, add all NUM_OF_INVENTORY_ITEMS items to the INVENTORY_ITEMS array and add their prices to the INVENTORY_PRICES array
+		for(int i = 0; i < NUM_OF_INVENTORY_ITEMS; i++) {
+			//Add the next line (item name) to the INVENTORY_ITEMS array
+			INVENTORY_ITEMS[i] = userInput.next();
+			//Add the next douvle (item cost) to the INVENTORY_PRICES array
+			INVENTORY_PRICES[i] = userInput.nextDouble();
+		}
+		
+		//Now, create a variable to get the number of customers that will be accounted for
+		int NUMBER_OF_CUSTOMERS = userInput.nextInt(); 
 
-		// Your code follows here.
-		
+		//For each customer, add the appropriate data to UNIQUE_CUSTOMERS and QUANTITY_PURCHASED
+		for (int i = 0; i < NUMBER_OF_CUSTOMERS; i++) {
+			//Discard names since they are irrelevant for A1Jedi
+			userInput.next();
+			userInput.next();
+			//Now, get the number of item entries that the current customer has purchased
+			int NUMBER_OF_ITEMS_PURCHASED = userInput.nextInt();
+			
+			//Create an array of booleans to see if the customer has already purchased the current item before since we don't want to count two transactions by the same customer as 2 distinct customers
+			boolean[] hasPurchasedThisItemAlready = new boolean[NUM_OF_INVENTORY_ITEMS];
+			//Before we look at the current customer, reset all the values in hasPurchasedThisItemAlready
+			clearBooleanArray(hasPurchasedThisItemAlready);
+			//Now loop over each item purchased and add its price to the table
+			for (int j = 0; j < NUMBER_OF_ITEMS_PURCHASED; j++) {
+				//Measure how many units the customer purchased of the current good
+				int quantityPurchased = userInput.nextInt();
+				//Get the index of the item so we can refer to the appropriate arrays
+				int itemIndex = getItemIndex(userInput.next(),INVENTORY_ITEMS);
+				//Check to see if the customer has already bought this item
+				if (!hasPurchasedThisItemAlready[itemIndex]) {
+					//If they have purchased the item, then update the value in hasPurchasedThisItemAlready
+					hasPurchasedThisItemAlready[itemIndex] = true;
+					//Increase the number of unique customers for this item
+					UNIQUE_CUSTOMERS[itemIndex]++;
+					//Increase the quantity of the item purchased by the appropriate amount
+					QUANTITY_PURCHASED[itemIndex] += quantityPurchased;
+				} else {
+					//If they have already purchased the item before, then only update its quantity purchased
+					QUANTITY_PURCHASED[itemIndex] += quantityPurchased;
+				}	
+			}
+		}
+		//Print the spending summary statistics now that all the data has been read and processed from the user
+		printSpendingStats(INVENTORY_ITEMS, UNIQUE_CUSTOMERS, QUANTITY_PURCHASED);
 	}
+	
+	/* clearBooleanArray
+	 * Takes an array of booleans and resets them all to be false
+	 * 
+	 * Input: An array of booleans (x)
+	 * 
+	 * Output: An array of booleans of equal length, all containing false values
+	 * 
+	 * Precondition:
+	 * Input array must not be null
+	 */
+	public static void clearBooleanArray(boolean[] x) {
+		for(int i = 0; i < x.length; i++) {
+			x[i]=false;
+		}
+	}
+	/* getItemIndex
+	 * Returns the index of an item in an array, given the item's name
+	 * 
+	 *  Input: An array of Strings (name); A String that has the name of the target item in the array (items)
+	 *  
+	 *  Output: An integer representing the items of 'name' in 'items'
+	 *  
+	 *  Precondition:
+	 *  Input array must not be null
+	 *  The key value must appear one time in the array that is being searched
+	 */
+	public static int getItemIndex(String name, String[] items) {
+		int currentIndex = 0;
+		for (int i = 0; i < items.length; i++) {
+			if (items[i].equals(name)) {
+				currentIndex = i;
+				break;
+			}
+		}
+		return currentIndex;
+	}
+	/* printSpendingStats
+	 * Prints the summary statistics about the items purchased in the A1 Jedi Format
+	 * 
+	 * Input: An array containing all of the item names (items)
+	 * 		  An array containing the number of unique consumers for each item (uniqueCustomers)
+	 * 		  An array containing the quantity of each item that was purchased (quantitiesPurchased)
+	 * 
+	 * Output: Summary statistics are printed to the console in A1 Jedi Format
+	 * 
+	 * Precondition:
+	 * All arrays must not be null
+	 * The index values in each of the arrays must be consistent in the following manner:
+	 * 		items[i] represents the name of an item
+	 * 		uniqueCustomers[i] represents the number of unique purchases of the item
+	 * 		quantitiesPurchased[i] represents the total number of units of the item that were sold
+	 */
+	public static void printSpendingStats(String[] items, int[] uniqueCustomers, int[] quantitiesPurchased) {
+		for (int i = 0; i < items.length; i++) {
+			if (uniqueCustomers[i] > 0) {
+				System.out.println(uniqueCustomers[i] + " customers bought " + quantitiesPurchased[i] + " " + items[i]);
+			} else {
+				System.out.println("No customers bought " + items[i]);
+			}
+		}
+	}
+	
+
 }
